6211b873415aadf18b2e9ddf250374d8fc00b48b
Andre <andre.rodriguez.cohort229@gmail.com>
2023-04-03 14:34:17 -0700

    first

---
+++ asynch-pt1.js
@@ -0,0 +1,22 @@
+/* Learning Questions
+1. What is the primary difference between synchronous and asynchronous 
+programming in JavaScript?
+A. Synchronous javascript is what we have been doing up until now.
+It runs from the top of the code to the bottom, it runs the task and result
+comes out when the task is done. Asynchronous code can
+run at any time in the background and while other code/functions run.
+
+2. Why is this useful?
+A. When you still want your program to run, while a certain task is being performed.
+
+3. What problems does it solve?
+A. It is very useful when tasks that take a a lot 
+
+            Notes
+I used event listeners in my final project for Module 2.
+Event handlers are a form of asynchronous programming 
+
+Event handlers are a type of callback function, or a function that gets passed on 
+to another function.
+*/
+
